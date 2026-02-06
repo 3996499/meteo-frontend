6b0a43699798b88dd193d461fd116b69848f26d2
Pc <3996499@alu.murciaeduca.es>
2026-02-06 17:34:40 +0100

    CORREGIR APP.JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // 🔹 Provincia
+  // Buscar Provincia
   const buscarProvincia = async (codigoProvincia) => {
     setLoading(true);
     setError(null);
@@ -41,7 +41,7 @@ function App() {
     }
   };
 
-  // 🔹 Municipio
+  // Buscar Municipio
   const buscarMunicipio = async (codigoMunicipio) => {
     if (!codigoMunicipio) return;
 
@@ -96,119 +96,4 @@ export default App;
 
 
 
-
-
-
-
-
-
-
-// import React from 'react';
-// import Buscador from './Components/BuscadorProvincia';
-// import Resultado from './Components/ResultadoProvincia';
-// import { useState } from 'react';
-// import './App.css';
-// import BuscadorMunicipio from './Components/BuscadorMunicipio';
-
-// function App() {
-//     const [data, setData] = useState(null);
-//     const [loading, setLoading] = useState(false);
-//     const [error, setError] = useState(null);
-//     const [municipioSeleccionado, setMunicipioSeleccionado] = useState(null);
-//     const [pronostico, setPronostico] = useState([]);
-//     const [municipios, setMunicipios] = useState([]);
-
-
-//     const buscar = async (codigoProvincia) => {
-//       setLoading(true);
-//       setError("");
-//       setData(null);
-
-//       try {
-//         const response = await fetch(
-//           `http://localhost:3000/api/tiempo/provincia/${codigoProvincia}`
-//         ); 
-
-//         if (!response.ok){
-//           throw new Error(`Error HTTP ${response.status}`);
-//         }
-
-
-//         const json = await response.json();
-        
-//         if (!json.success){
-//           throw new Error(json.message || "Error al obtener datos");
-//         }
-
-//         setData(json.data);
-//       } catch (error) {
-//         setError(error.message);
-//       } finally {
-//         setLoading(false);
-//     }
-// };
-
-// const buscarMunicipio = async (codigoMunicipio) => {
-//   setLoading(true);
-//   setError(null);
-//   setPronostico(null);
-
-//   try {
-//     const response = await fetch(
-//       `http://localhost:3000/api/tiempo/municipio/${codigoMunicipio}`
-//     ); 
-
-//     if (!response.ok){
-//       throw new Error(`Error HTTP ${response.status}`);
-//     }
-
-
-//     const json = await response.json();
-    
-//     if (!json.success){
-//       throw new Error(json.message || "Error al obtener datos");
-//     }
-
-//     setPronostico(json.data);
-//   } catch (error) {
-//     setError(error.message);
-//   } finally {
-//     setLoading(false);
-// }
-// };
-
-
-// const cargarMunicipios = async (codigoProvincia) => {
-//   try {
-//     const response = await fetch(
-//       `http://localhost:3000/api/municipios/${codigoProvincia}`
-//     );
-
-//     const json = await response.json();
-//     console.log("Municipios recibidos:", json);
-
-//     if (json.success) {
-//       setMunicipios(json.data);
-//     } else {
-//       setMunicipios([]);
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     setMunicipios([]);
-//   }
-// };
-
-
-
-// return (
-//   <div className='app'>
-//     <h1>Buscador de tiempo AEMET</h1>
-//     <Buscador onBuscar={buscar} />
-//     <Resultado loading={loading} error={error} data={data} />
-    
-//   </div>
-// );
-// }
-
-// export default App;
 
