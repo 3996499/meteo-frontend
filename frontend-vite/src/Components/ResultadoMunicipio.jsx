820a6301fefe8a59853c8ad3542595054cda5e23
Pc <3996499@alu.murciaeduca.es>
2026-02-10 10:08:12 +0100

    modificar salida de fecha en municipios

--- frontend-vite/src/Components/ResultadoMunicipio.jsx
+++ frontend-vite/src/Components/ResultadoMunicipio.jsx
@@ -1,16 +1,30 @@
-export default function ResultadoMunicipio({ data, municipio }) {
+function formatearFecha(fechaISO) {
+  const fecha = new Date (fechaISO);
+  return fecha.toLocaleDateString("es-ES", {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  });
+}
+
+export default function ResultadoMunicipio({data, municipio}) {
   if (!Array.isArray(data) || data.length === 0) return null;
 
   return (
     <div>
-      <h2>Predicción 7 días </h2>
-  <ul className="pronostico">
-  {data.map((dia, index) => (
-    <li key={index}>
-       📅 <strong>{dia.fecha}</strong> — 🌡️ {dia.tmin}°C / {dia.tmax}°C · ☁️ {dia.estadoCielo} · 🌧️ {dia.precipitacion}%
-    </li>
-  ))}
-</ul>
+      <h2>Predicción 7 días</h2>
+
+      <ul className="pronostico">
+        {data.map((dia, index) => (
+          <li key={index}>
+            📅 <strong>{formatearFecha(dia.fecha)}</strong> — 🌡️ {dia.tmin}°C / {dia.tmax}°C · ☁️ {dia.estadoCielo} · 🌧️ {dia.precipitacion}%
+          </li>
+        ))}
+          
+        
+      </ul>
+
+
     </div>
-  );
+  )
 }
